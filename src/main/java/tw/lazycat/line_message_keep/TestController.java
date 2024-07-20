b76c314ae25954f4b9278a438bba4ac7650a2331
tim12367 <tim12367@ymail.com>
2024-07-20 19:52:17 +0800

    add test controller

---
+++ src/main/java/tw/lazycat/line_message_keep/TestController.java
@@ -0,0 +1,17 @@
+package tw.lazycat.line_message_keep;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class TestController {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		log.debug("hi!");
+		return "Hello World";
+	}
+}
