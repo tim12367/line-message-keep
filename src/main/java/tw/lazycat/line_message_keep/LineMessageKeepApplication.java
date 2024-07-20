3dc40c5c10e793f4bf92e6b808d437ad0dda7a7a
tim12367 <tim12367@ymail.com>
2024-07-20 14:51:11 +0800

    init commit

---
+++ src/main/java/tw/lazycat/line_message_keep/LineMessageKeepApplication.java
@@ -0,0 +1,13 @@
+package tw.lazycat.line_message_keep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineMessageKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineMessageKeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/lazycat/line_message_keep/LineMessageKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.lazycat.line_message_keep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineMessageKeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
